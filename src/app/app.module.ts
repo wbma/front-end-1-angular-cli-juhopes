c3682778a24c70361073fa6b6e5beec9a19dd30d
juhopes <juhopessi1@gmail.com>
2018-01-16 12:48:13 +0200

    week 1 task1 B

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MaincontentComponent } from './maincontent/maincontent.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    MaincontentComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
